064c58a09642b2e1b626b27dfd267075d01182dc
Vimal Vs <vimalchan24@gmail.com>
2023-09-12 00:47:51 +0530

    header&footer changes

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,58 @@
+import bg from './assets/footer_bg_res.jpg'
+import logo from './assets/logo_notext.svg'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { FiMail } from "react-icons/fi";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { FaLocationDot } from "react-icons/fa6";
+
+import { poppinsRegular } from '@/app/fonts';
+
+export default function Footer() {
+    return (
+        <>
+            <div className={`${poppinsRegular.className} absolute text-white z-10 flex flex-col md:flex-row items-center min-h-[50vh] w-full`}>
+                <div className='md:w-[30%] pt-8 md:pt-0 flex flex-col gap-4 justify-center items-center'>
+                    <Link href={"/"} >
+                        <Image src={logo} alt="logo" className="h-20" />
+                    </Link>
+                    <p className='font-semibold text-2xl'>FarmSense</p>
+                </div>
+                <div className='bg-white h-[40vh] w-[1px] hidden md:inline-block' />
+                <div className='md:w-[70%] h-[40vh] flex flex-col justify-between'>
+                    <div className='flex gap-4 justify-evenly pt-12 md:pt-8'>
+                        <div className='flex flex-col gap-8 items-start '>
+                            <p className='text-xl uppercase text-gray-300 font-semibold hidden md:inline-block'>Contact</p>
+                            <Link href={"mailto:farmsense@gmail.com"} className='flex gap-4 justify-center items-center'><FiMail /><p>farmsense@gmail.com</p></Link>
+                            <Link href={"tel:+917010537101"} className='flex gap-4 justify-center items-center'><BsFillTelephoneFill /><p>+91 70105 37101</p></Link>
+                            <Link href={"https://goo.gl/maps/FEWhmjKYhc565eSY8"} className='flex gap-4 justify-center items-center'><FaLocationDot /><p>Tiruchirappalli, Tamil Nadu</p></Link>
+                        </div>
+                        <div className='hidden md:flex flex-col gap-8'>
+                            <p className='text-xl uppercase text-gray-300 font-semibold'>Additional Links</p>
+                            <ul className='flex flex-col justify-center gap-6 text-lg'>
+                                <li><Link href={""}>Documentation</Link></li>
+                                <li><Link href={""}>Cookies</Link></li>
+                                <li><Link href={""}>Privacy & Policy</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-2 md:gap-6 py-4'>
+                        <p className='hidden md:inline-block text-center md:text-lg text-gray-300'>
+                            Copyright © 2023 FarmSense | All Rights Reserved
+                        </p>
+                        <p className='md:hidden text-center md:text-lg text-gray-300'>
+                            Copyright © 2023 FarmSense
+                        </p>
+                        <p className='md:hidden text-center md:text-lg text-gray-300'>All Rights Reserved</p>
+                    </div>
+                </div>
+            </div>
+            <>
+                <div className="absolute w-full min-h-[60vh] md:min-h-[50vh] bg-black opacity-60" />
+                <Image src={bg} alt='img' className='md:h-[50vh] h-[60vh] w-full object-cover' />
+            </>
+        </>
+    )
+}
---
+++ src/components/layout/NavBar.jsx
@@ -0,0 +1,120 @@
+'use client'
+
+import logo from './assets/logo.svg'
+
+import Image from "next/image"
+import Link from "next/link"
+
+import { inder } from "@/app/fonts"
+import { usePathname } from "next/navigation"
+import { motion } from "framer-motion"
+import { useEffect, useRef, useState } from "react"
+
+export default function NavBar() {
+    const router = usePathname();
+    const [viewNav, setViewNav] = useState(false);
+
+    const handleViewNav = () => {
+        if (viewNav === false) {
+            setViewNav(true);
+        } else {
+            setViewNav(false);
+        }
+    };
+    const navVisibleRef = useRef();
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                navVisibleRef.current &&
+                !navVisibleRef.current.contains(event.target)
+            ) {
+                setViewNav(false);
+            }
+        };
+        document.addEventListener("click", handleClickOutside);
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <nav className="flex justify-between items-center h-[8vh] px-8 bg-primary-green w-full">
+            <Link href={"/"}>
+                <Image src={logo} alt="logo" className="h-12 md:h-16 w-max" />
+            </Link>
+            <ul className={`hidden md:flex justify-around text-white font-poppins gap-20 ${inder.className} text-2xl`}>
+                <li><Link href={"/"}
+                    className={router === "/" ? 'text-[#09F78B]' : ""}>
+                    Home
+                </Link></li>
+                <li><Link href={"/about"}
+                    className={router === "/about" ? 'text-[#09F78B]' : ""}>
+                    About</Link></li>
+                <li><Link href={"/dashboard"}
+                    className={router === "/dashboard" ? 'text-[#09F78B]' : ""}>
+                    Dashboard</Link></li>
+            </ul>
+            <button
+                className="md:hidden cursor-pointer space-y-[6px]"
+                onClick={handleViewNav}
+                ref={navVisibleRef}
+            >
+                <div
+                    className={`w-6 h-0.5 bg-white ${viewNav && "rotate-45"
+                        } transition-all duration-200`}
+                ></div>
+                <div
+                    className={`w-4 h-0.5 bg-white ml-auto ${viewNav && "hidden"}`}
+                ></div>
+                <div
+                    className={`w-6 h-0.5 bg-white ${viewNav && "-rotate-45 -translate-y-2"
+                        } transition-all duration-200`}
+                ></div>
+            </button>
+            {viewNav &&
+                <motion.div className="fixed mt-2 py-2 pb-12 right-0 bg-white text-black z-20 shadow-md top-[60px] h-1/3 w-full"
+                    variants={{
+                        open: {
+                            x: "0%",
+                        },
+                        closed: {
+                            x: "100%",
+                        },
+                    }}
+                    initial="closed"
+                    animate="open"
+                    exit="closed"
+                    transition={{
+                        ease: "easeOut",
+                        duration: 0.3,
+                    }}
+                >
+                    <ul className="flex flex-col justify-evenly h-full font-medium text-center text-lg px-4">
+                        <li
+                            className={`px-4 py-2 rounded-md`}
+                        >
+                            <Link href="/" className={router === "/" ? 'text-[#35b179]' : ""}>
+                                Home
+                            </Link>
+                        </li>
+                        <li
+                            className={`px-4 py-2 rounded-md`}
+                        >
+                            <Link href="/about" className={router === "/about" ? 'text-[#35b179]' : ""}>
+                                About
+                            </Link>
+                        </li>
+                        <li
+                            className={`px-4 py-2 rounded-md`}
+                        >
+                            <Link href="/dashboard" className={router === "/dashboard" ? 'text-[#35b179]' : ""}>
+                                Dashboard
+                            </Link>
+                        </li>
+                    </ul>
+                </motion.div>
+            }
+        </nav>
+    )
+}
