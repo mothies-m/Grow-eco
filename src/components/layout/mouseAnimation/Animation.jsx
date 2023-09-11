3798969a5e68d722b4b7b6c5df9a71c7d367f628
Vimal Vs <vimalchan24@gmail.com>
2023-09-12 00:48:57 +0530

    hero-banner animation

---
+++ src/components/layout/mouseAnimation/Animation.jsx
@@ -0,0 +1,10 @@
+import "./Animation.css"
+
+export default function Animation() {
+    return (
+        <>
+            <div className="mouse hidden md:inline-block" />
+            <div className="scroll md:hidden" />
+        </>
+    )
+}
