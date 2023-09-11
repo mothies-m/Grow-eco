d5151294857e82baefacc0ad93780003187b807d
Vimal Vs <vimalchan24@gmail.com>
2023-09-12 00:47:25 +0530

    fonts imported

---
+++ src/app/fonts.jsx
@@ -0,0 +1,34 @@
+import { Inter, Inder, Montserrat, Raleway, Poppins } from 'next/font/google'
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const inder = Inder({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: "400"
+})
+
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    display: 'swap'
+})
+
+export const raleway = Raleway({
+    subsets: ['latin'],
+    display: 'swap'
+})
+
+export const poppinsRegular = Poppins({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: "400"
+})
+
+export const poppinsBold = Poppins({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: "700"
+})
