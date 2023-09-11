2b6672288d7c47001e89c6c4e6aa275c8ea7204b
Vimal Vs <vimalsakkthi.developer@gmail.com>
2023-09-12 04:00:36 +0530

    content width increase

--- src/components/home/TextBox.jsx
+++ src/components/home/TextBox.jsx
@@ -9,7 +9,7 @@ export default function TextBox() {
 
     return (
         <div className="flex justify-center items-center bg-[#EBEBEB] w-full min-h-[60vh] md:min-h-[50vh] my-20 py-8 px-4 md:px-0">
-            <div className="flex flex-col justify-evenly gap-8 h-full md:w-2/3">
+            <div className="flex flex-col justify-evenly gap-8 h-full md:w-3/4">
                 <h3 className={`text-3xl md:text-4xl lg:text-6xl text-[#575757] leading-[2.6rem] lg:leading-[5rem] ${poppinsBold.className}`}>{content.heading}</h3>
                 <p className={`text-lg md:text-xl lg:text-2xl text-[#7B7B7B] lg:w-5/6 leading-[2.2rem] lg:leading-[3rem] ${poppinsRegular.className}`}>{content.paragraph}</p>
             </div>
