38b0678979a1b584e9b483e7c6a5518382c6941d
Vimal Vs <vimalsakkthi.developer@gmail.com>
2023-09-12 11:29:48 +0530

    firebase-client added

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -7,7 +7,7 @@ import Weather from "@/components/dashboard/Weather";
 export default function page() {
     return (
         <div className={`flex flex-col gap-6 my-12 px-4`}>
-            <p className={`text-xl font-semibold ${poppinsRegular.className}`}>This is a sample page from a Farmer's perspective, not a live page.</p>
+            <p className={`text-xl font-semibold ${poppinsRegular.className}`}>This is a sample page from a Farmer&apos;s perspective, not a live page.</p>
             <Image src={dash} alt="dashboard" />
             <Weather />
         </div>
