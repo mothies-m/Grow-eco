c4e56d2532c56d763a1454f8fc2ab114a63939ea
Vimal Vs <vimalsakkthi.developer@gmail.com>
2023-09-12 12:08:27 +0530

    api res changes

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import dash from './assets/dash.png'
 import { poppinsRegular } from "../fonts";
+import { AiOutlineWarning } from "react-icons/ai";
 import Weather from "@/components/dashboard/Weather";
 
 
 export default function page() {
     return (
-        <div className={`flex flex-col gap-6 my-12 px-4`}>
-            <p className={`text-xl font-semibold ${poppinsRegular.className}`}>This is a sample page from a Farmer&apos;s perspective, not a live page.</p>
+        <div className={`flex flex-col items-center gap-6 my-12 px-4`}>
+            <p className={`flex items-center gap-2 text-xl text-start font-semibold ${poppinsRegular.className}`}><span className="text-3xl"><AiOutlineWarning /></span> This is a sample dashboard from a Farmer&apos;s perspective, not a live page.</p>
             <Image src={dash} alt="dashboard" />
             <Weather />
         </div>
