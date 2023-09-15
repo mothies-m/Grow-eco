07c949a2407fcd802ee33b7499e75c6cfa1fb111
Vimal Vs <vimalsakkthi.developer@gmail.com>
2023-09-15 23:41:03 +0530

    toast added

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,16 +1,35 @@
+'use client'
+
 import { poppinsRegular } from "../fonts";
-import { AiOutlineWarning } from "react-icons/ai";
 import Weather from "@/components/dashboard/Weather";
 import DashboardGraph from "@/components/dashboard/DashboardGraph";
-
+import DashboardGraphTamil from "@/components/dashboard/DashboardGraphTamil";
+import DashboardGraphHindi from "@/components/dashboard/DashboardGraphHindi";
+import { BsGlobe } from "react-icons/bs";
+import { useState } from "react";
 
 export default function page() {
 
+    const [language, setLanguage] = useState("english");
+    const handleChange = (e) => {
+        setLanguage(e.target.value);
+    }
     return (
-        <div className={`flex flex-col items-center gap-6 my-12 px-4`}>
-            <p className={`flex items-center gap-2 text-xl text-start font-semibold ${poppinsRegular.className}`}><span className="text-3xl"></span>This is the dashboard from a Farmer&apos;s perspective.</p>
-            <DashboardGraph />
-            <Weather />
-        </div>
+        <>
+            <label htmlFor="language" className="float-right">
+                <BsGlobe />
+            </label>
+            <select name="language" className="p-2 border-2 border-primary-green text-lg right-0 absolute rounded-md" defaultValue={"english"} onChange={handleChange}>
+                <option value="english" >English</option>
+                <option value="tamil" >Tamil</option>
+                <option value="hindi" >Hindi</option>
+            </select >
+            <div className={`flex flex-col items-center gap-6 my-12 px-4`}>
+                {(language === 'english') && <DashboardGraph />}
+                {(language === 'tamil') && <DashboardGraphTamil />}
+                {(language === 'hindi') && <DashboardGraphHindi />}
+                <Weather />
+            </div>
+        </>
     )
 }
