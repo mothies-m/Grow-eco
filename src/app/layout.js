0494611126bb7f7bdd3a92c9b6cfbaef19d18eb9
drig0 <62328695+mothies-m@users.noreply.github.com>
2023-11-02 08:22:53 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import Footer from '@/components/layout/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'FarmSense',
+  title: 'GrowEco',
   description: 'Revolutionize Agriculture with Modern Technology',
 }
 
